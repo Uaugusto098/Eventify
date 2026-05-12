749d94a86741c386faab9ea503ee4182531baf4d
Pedro <pedroluchese@gmail.com>
2026-05-11 21:07:41 -0300

    Firebase conectado!

--- app/src/main/java/com/example/eventlyapp/MainActivity.java
+++ app/src/main/java/com/example/eventlyapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.eventlyapp;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("teste_conexao");
+
+        myRef.setValue("Opa, Pedro! Conectado via Java.")
+                .addOnSuccessListener(aVoid -> {
+                    Log.d("FIREBASE_TESTE", "Conexão realizada com sucesso!");
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("FIREBASE_TESTE", "Erro ao conectar: " + e.getMessage());
+                });
     }
 }
